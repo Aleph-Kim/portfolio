d4181eeaeeb7dc108008d3268d98a0a377c6f4ff
김채민 <dktjdej25@gmail.com>
2024-12-03 22:47:31 +0900

    새 기능 - 다크모드 지원

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
     <div id="app">
+        <span @click="toggleDarkMode" class="fixed top-3 right-3 w-7 h-7 cursor-pointer">
+            <div v-if="isDarkMode">
+                <!-- 달 아이콘 -->
+                <svg className="bg-black" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+            </div>
+            <div v-else>
+                <!-- 해 아이콘 -->
+                <svg className="bg-white" fill="black" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z">
+                    </path>
+                </svg>
+            </div>
+        </span>
         <nav
-            :class="['w-full fixed bottom-0 flex justify-center bg-white/70 h-14 transition-opacity duration-700', { 'opacity-0 pointer-events-none': !isNavbarVisible }]">
+            :class="['w-full fixed bottom-0 flex justify-center bg-white/70 dark:bg-[#181818]/70 h-14 transition-all ease-[ease] duration-500', { 'opacity-0 pointer-events-none': !isNavbarVisible }]">
             <div class="w-[70%] flex justify-center items-center">
                 <a v-for="(section, index) in sections" :key="index" @click="scrollToSection(index + 1)" :class="['w-40 text-center hover:cursor-pointer hover:text-xl transition-all',
                     currentSection === index + 1 ? 'font-bold' : '']">
@@ -15,6 +31,7 @@
         <Section4 ref="Section4" />
     </div>
 </template>
+
 <script>
 import Home from './components/Home.vue';
 import About from './components/About.vue';
@@ -29,9 +46,33 @@ export default {
             sections: ['Home', 'About', 'Career', 'Portfolio'], // 네비게이션 텍스트
             isNavbarVisible: true, // 네비게이션 노출 여부
             scrollTimeout: null, // 타이머 ID
+            isDarkMode: false,  // 다크 모드 상태
+
         };
     },
     methods: {
+        // 다크 모드 상태 설정, 로컬 스토리지에 저장 메서드
+        setDarkMode(isDark) {
+            this.isDarkMode = isDark;
+            document.documentElement.classList.toggle('dark', isDark); // 다크 모드 CSS 클래스 토글
+            localStorage.setItem('darkMode', isDark.toString()); // 다크 모드 상태를 로컬 스토리지에 저장
+        },
+        // 테마 상태 체크 메서드
+        checkTheme() {
+            const savedDarkMode = localStorage.getItem('darkMode'); // 로컬 스토리지에서 다크 모드 상태를 읽음
+            if (savedDarkMode !== null) {
+                // 로컬 스토리지에 저장된 상태가 있으면 그 상태를 사용
+                this.setDarkMode(savedDarkMode === 'true');
+            } else {
+                // 저장된 상태가 없으면 OS 기본 테마를 확인
+                const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                this.setDarkMode(prefersDarkMode);
+            }
+        },
+        // 다크 모드 상태 토글 메서드
+        toggleDarkMode() {
+            this.setDarkMode(!this.isDarkMode); // 현재 상태의 반대값으로 설정
+        },
         scrollToSection(sectionNumber) {
             this.$refs[`Section${sectionNumber}`].$el.scrollIntoView({ behavior: 'smooth' });
             this.currentSection = sectionNumber; // 클릭한 섹션을 현재 섹션으로 설정
@@ -70,6 +111,7 @@ export default {
         },
     },
     mounted() {
+        this.checkTheme(); // 페이지 로드 시 다크 모드 상태 확인
         window.addEventListener('scroll', () => {
             this.updateCurrentSection();
             this.showNavbar();
