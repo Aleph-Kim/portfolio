d4544b75463c310d0036ff9c7f32ac5ff6fd2a40
Aleph kim <dktjdej@naver.com>
2025-06-20 19:06:45 +0900

    수정 - About 데이터 생년월일 수정

--- public/data/about.js
+++ public/data/about.js
@@ -3,7 +3,7 @@ export const aboutData = {
     "title": "웹 개발자(BE, FE)",
     "description": "소통이 잘 되는 개발자, 함께 일하고 싶은 개발자가 되기위해 노력합니다.",
     "phone": "010-9559-1750",
-    "birthday": "00.06.17",
+    "birthday": "2000.06.17",
     "email": "dktjdej@naver.com",
     "github": "https://github.com/aleph-kim",
     "blog": "https://dev-kimchi.tistory.com",
